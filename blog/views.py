1d55a22c6dcf460a1cda58a9a46d1a073ea17daf
Hiram Torres <hiram.torres16@my.unitec.edu.mx>
2023-02-01 23:36:51 -0600

    Agregué contenido a proyecto

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
-from .models import Post
+from django.shortcuts import render, get_object_or_404 # Para mostrar un error 404 si no encuentra la categoría
+from .models import Post, Category
 # Create your views here.
 
 def blog(request):
     posts = Post.objects.all()
     return render(request,"blog/blog.html", {'posts':posts})
+
+def category(request, category_id):
+    category = get_object_or_404(Category,id=category_id)
+    return render(request, "blog/category.html", {'category':category})
